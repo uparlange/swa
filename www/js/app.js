cc0286221ed69a7bf148645b3e47ab10acd0bf21
Ugo Parlange <uparlange@gmail.com>
2018-01-24 22:07:14 +0100

    Refactoring images

--- www/js/app.js
+++ www/js/app.js
@@ -5,23 +5,23 @@ const HomeView = Fwk.util.ComponentUtils.getComponent("home-view", {
                 {
                     labelKey: "LABEL_CLIENT",
                     items: [
-                        { link: "https://vuejs.org/", image: "/images/vuejs.png", description: "Vue.js" },
-                        { link: "https://vuetifyjs.com/", image: "/images/vuetify.png", description: "Vuetify" }
+                        { link: "https://vuejs.org/", image: "/images/stack/vuejs.png", description: "Vue.js" },
+                        { link: "https://vuetifyjs.com/", image: "/images/stack/vuetify.png", description: "Vuetify" }
                     ]
                 },
                 {
                     labelKey: "LABEL_MIDDLEWARE",
                     items: [
-                        { link: "https://github.com/auth0/node-jsonwebtoken", image: "/images/jwt.png", description: "JSON Web Tokens" },
-                        { link: "http://www.passportjs.org/", image: "/images/passportjs.png", description: "Passport" },
-                        { link: "http://expressjs.com/", image: "/images/expressjs.png", description: "Express" },
-                        { link: "https://nodejs.org", image: "/images/nodejs.png", description: "Node.js" }
+                        { link: "https://github.com/auth0/node-jsonwebtoken", image: "/images/stack/jwt.png", description: "JSON Web Tokens" },
+                        { link: "http://www.passportjs.org/", image: "/images/stack/passportjs.png", description: "Passport" },
+                        { link: "http://expressjs.com/", image: "/images/stack/expressjs.png", description: "Express" },
+                        { link: "https://nodejs.org", image: "/images/stack/nodejs.png", description: "Node.js" }
                     ]
                 },
                 {
                     labelKey: "LABEL_DATABASE",
                     items: [
-                        { link: "https://pouchdb.com/", image: "/images/pouchdb.png", description: "PouchDB" }
+                        { link: "https://pouchdb.com/", image: "/images/stack/pouchdb.png", description: "PouchDB" }
                     ]
                 }
             ]
