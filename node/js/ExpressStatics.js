df505970db3a877a840ad39163841e7d50c81520
Ugo Parlange <uparlange@gmail.com>
2018-01-30 23:18:22 +0100

    Remove test line

--- node/js/ExpressStatics.js
+++ node/js/ExpressStatics.js
@@ -6,7 +6,6 @@ const Config = require("./Config");
 
 // exported methods
 exports.init = function (instance) {
-    process.env.NODE_ENV = "production";
     const www = (process.env.NODE_ENV === "production") ? "www/prod" : "www/dev";
     instance.use(express.static(www));
     Config.getConfig().vendors.forEach((item, index, array) => {
