c3a26b6b9009840286bd2dea6ccb003a0d20e148
Ugo Parlange <uparlange@gmail.com>
2018-02-03 17:49:02 +0100

    Try pouchdb-adapter-node-websql

--- src-back/js/PouchDB.js
+++ src-back/js/PouchDB.js
@@ -2,11 +2,15 @@
 const PouchDB = require("pouchdb-node");
 PouchDB.plugin(require("pouchdb-find"));
 //PouchDB.plugin(require("pouchdb-adapter-memory"));
+PouchDB.plugin(require("pouchdb-adapter-node-websql"));
 //PouchDB.debug.enable("pouchdb:find");
 
 // exported methods
 exports.getDatabase = function (dbName) {
+    /*
     const path = __dirname + "/../data/db/" + dbName;
-    //return new PouchDB(dbName, { adapter: "memory" });
-    return new PouchDB(path);
+    return new PouchDB(dbName, { adapter: "memory" });
+    */
+    const path = __dirname + "/../data/db/" + dbName + ".db";
+    return new PouchDB(path, { adapter: "websql" });
 };
